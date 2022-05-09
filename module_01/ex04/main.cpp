4900cfc053b8b5e6092c11460e097ea994b0242a
Mohammed Patel <patmohd@hotmail.com>
2022-05-09 04:14:30 +0400

    error handling ex04

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -11,14 +11,28 @@ int main(int ac, char **av)
 	std::string			output_name;
 	std::size_t			pos;
 
+	if (ac != 4)
+	{
+		std::cout << "Wrong number of arguments" << std::endl;
+		return (1);
+	}
+	if (av[2] == 0 || av[3] == 0)
+	{
+		std::cout << "Empty strings provided" << std::endl;
+		return (1);
+	}
 	input.open(av[1], std::fstream::in);
 	if (!input.is_open())
+	{
+		std::cout << "Error opening file" << std::endl;
 		return (1);
+	}
 	output_name = av[1];
 	output_name += ".replace";
 	output.open(output_name.c_str(), std::fstream::out|std::fstream::trunc);
 	if (!output.is_open())
 	{
+		std::cout << "Error creating file " << av[1] << ".replace" << std::endl;
 		input.close();
 		return (1);
 	}
